75f337f14f483aeb6d1c18a1d7ad4aa3c030819d
VictorHugo110199 <victorhugo110199@gmail.com>
2023-03-07 09:18:27 -0300

    inicial commit

--- copias/views.py
+++ copias/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import CopySerializer
+from .models import Copy
+
+class CopyView (CreateAPIView):
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+class CopyDetailView (RetrieveUpdateDestroyAPIView, ListCreateAPIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
 
 # Create your views here.
