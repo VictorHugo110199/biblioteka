591d305a7dafd590290e14c7289479aa33474813
VictorHugo110199 <victorhugo110199@gmail.com>
2023-03-10 13:59:03 -0300

    fet: CRUD copy

--- copias/serializers.py
+++ copias/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import Copy
 
 class CopySerializer(serializers.ModelSerializer):
+
+    # books_id = serializers.CharField(validators = [
+    #     UniqueValidator(
+    #         queryset=Copy.objects.all(),
+    #         message='This book already has a copy.'
+    #     )]
+    # )
     class Meta:
         model = Copy
-        fields = ["amount", "books"]
+        fields = ["id", "amount", "books_id", "copy_booked", "is_available"]
--- copias/views.py
+++ copias/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from users.permissions import IsCollaborator
 from .serializers import CopySerializer
 from .models import Copy
+from livros.permissions import isAdminOrGetOnly
+from django.shortcuts import get_object_or_404
+from livros.models import Book
 
-class CopyView (CreateAPIView):
+
+class CopyView(ListCreateAPIView):
     queryset = Copy.objects.all()
     serializer_class = CopySerializer
 
+    def perform_create(self, serializer):
+        book = get_object_or_404(Book, pk=self.request.data['books_id'])
+        serializer.save(books = book)
+
+    permission_classes = [isAdminOrGetOnly]
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsCollaborator]
 
-class CopyDetailView (RetrieveUpdateDestroyAPIView):
-    queryset = Copy.objects.all()
-    serializer_class = CopySerializer
-
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsCollaborator]
-
-
-class CopyListView (ListCreateAPIView):
+class CopyDetailsView(RetrieveUpdateDestroyAPIView):
     queryset = Copy.objects.all()
     serializer_class = CopySerializer
     
+    permission_classes = [isAdminOrGetOnly]
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsCollaborator]
 
 
 # Create your views here.
