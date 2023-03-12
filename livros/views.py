881a07e22884aba8859dd87779a30e16df4d0acd
Lucas Mitori <103422552+LucasMitori@users.noreply.github.com>
2023-03-12 12:33:04 -0300

    feat/ added functions of follow, list follow | doc swagger

--- livros/views.py
+++ livros/views.py
@@ -1,14 +1,13 @@
 from rest_framework.views import APIView, Response, Request, status
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import BookSerializer
+from .serializers import BookSerializer, BookFollowSerializer
 from .models import Book
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permissions import isAdminOrGetOnly
 from django.shortcuts import get_object_or_404
 from users.models import User
 from .tasks import send_notification
-
 
 
 class BookView(ListCreateAPIView):
@@ -39,4 +38,31 @@ class FollowingView(APIView):
         send_notification(user, book)
 
         return Response({"message": f"Você está seguindo o livro {book.title}!"}, status.HTTP_201_CREATED)
+    
+    def delete(self, request: Request, book_id:int) -> Response:
 
+        user_requester = request.user.id
+
+        book = get_object_or_404(Book, id=book_id)
+        user = get_object_or_404(User, id=user_requester)
+
+        book.following.remove(user)
+        send_notification(user, book)
+
+        return Response(status.HTTP_204_NO_CONTENT)
+
+
+class FollowingDetailView(APIView):
+    authentication_classes= [JWTAuthentication]
+
+    def get(self, request:Request) -> Response:
+        user = get_object_or_404(User, id=request.user.id)
+
+        book_followed = Book.objects.filter(
+            following=user
+        )
+
+        serializer = BookFollowSerializer(book_followed, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+    
