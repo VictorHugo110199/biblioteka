70985ae395de093285520b702312ec8234904250
brunobbittencourt <bbittencourt@outlook.com.br>
2023-03-13 19:58:14 -0300

    feat: borrow and return routes done

--- copias/serializers.py
+++ copias/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from .models import Copy
+from .models import Copy, Borrow
 from livros.models import Book
 from django.forms.models import model_to_dict
+from users.serializers import UserSerializer
+from users.models import User
 
 class CopySerializer(serializers.ModelSerializer):
     book = serializers.SerializerMethodField(read_only=True)
@@ -23,3 +25,30 @@ class CopySerializer(serializers.ModelSerializer):
         returned_book = model_to_dict(found_book)
         returned_book.pop('following')
         return returned_book
+
+class BorrowSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Borrow
+        fields = ["id", "user", "copy", "borrowing_start_date", "return_date", "is_returned"]
+        read_only_fields = ["id", "user", "copy", "borrowing_start_date", "return_date", "is_returned"]
+
+    def update(self, instance, validated_data):
+        copy = Copy.objects.filter(pk=instance.copy.id).first()
+
+        copy.copy_booked -= 1
+
+        copy.is_available = True
+
+        copy.save()
+
+        instance.is_returned=True
+
+        instance.save()
+
+        user = User.objects.filter(pk=validated_data["user"].id).first()
+
+        user.is_allowed = True
+        user.save()
+
+        return instance
--- copias/urls.py
+++ copias/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("emprestar/<int:pk>/", views.BorrowView.as_view()),
-    path("devolver/<int:pk>/", views.BorrowDetailView.as_view()),
+    path("borrow/history/", views.BorrowListView.as_view()),
+    path("borrow/history/<int:pk>/", views.BorrowListCollaboratorView.as_view()),
+    path("borrow/<int:pk>/", views.BorrowView.as_view()),
+    path("return/<int:pk>/", views.BorrowDetailView.as_view()),
     path("copy/", views.CopyView.as_view()),
     path("copy/<pk>/", views.CopyDetailsView.as_view()),
 ]
